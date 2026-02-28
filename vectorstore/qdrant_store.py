79eb1273e508940cf686418628c05f0c0795df31
devopriyanshu <priyanshupatel1902@gmail.com>
2026-03-01 01:12:09 +0530

    qdrant cloud

--- vectorstore/qdrant_store.py
+++ vectorstore/qdrant_store.py
@@ -8,12 +8,18 @@ COLLECTION_NAME = 'youtube_videos'
 
 def get_vector_store():
     qdrant_url = os.getenv("QDRANT_URL", "http://localhost:6333")
-    client = QdrantClient(url=qdrant_url)
+    qdrant_api_key = os.getenv("QDRANT_API_KEY")
+    client = QdrantClient(url=qdrant_url, api_key=qdrant_api_key)
 
     if not client.collection_exists(COLLECTION_NAME):
         client.create_collection(
             collection_name=COLLECTION_NAME,
             vectors_config=models.VectorParams(size=384, distance=models.Distance.COSINE),
+        )
+        client.create_payload_index(
+            collection_name=COLLECTION_NAME,
+            field_name="metadata.video_id",
+            field_schema=models.PayloadSchemaType.KEYWORD,
         )
 
     return QdrantVectorStore(
@@ -24,7 +30,8 @@ def get_vector_store():
 
 def check_video_exists(video_id: str) -> bool:
     qdrant_url = os.getenv("QDRANT_URL", "http://localhost:6333")
-    client = QdrantClient(url=qdrant_url)
+    qdrant_api_key = os.getenv("QDRANT_API_KEY")
+    client = QdrantClient(url=qdrant_url, api_key=qdrant_api_key)
     try:
         res = client.scroll(
             collection_name=COLLECTION_NAME,
